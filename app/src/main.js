6c31dc95ef353cc1a96311a2327571d230a27e8c
Danjavia <danjavia.iconshock@gmail.com>
2014-04-22 19:01:27 +0000

    second commit, styles are missed

--- app/src/main.js
+++ app/src/main.js
@@ -1,23 +1,49 @@
 /*globals define*/
-define(function(require, exports, module) {
+define( function( require, exports, module ) {
+    
     'use strict';
     // import dependencies
-    var Engine = require('famous/core/Engine');
-    var ImageSurface = require('famous/surfaces/ImageSurface');
-    var StateModifier = require('famous/modifiers/StateModifier');
+    var Engine = require( 'famous/core/Engine' );
+    var Surface = require( 'famous/core/Surface' );
+    var ImageSurface = require( 'famous/surfaces/ImageSurface' );
+    var StateModifier = require( 'famous/modifiers/StateModifier' );
+	var HeaderFooterLayout = require("famous/views/HeaderFooterLayout");
+	var GridLayout = require("famous/views/GridLayout");
 
     // create the main context
     var mainContext = Engine.createContext();
 
-    // your app here
-    var logo = new ImageSurface({
-        size: [200, 200],
-        content: 'content/images/famous_logo.png'
+    var layout = new HeaderFooterLayout({
+        headerSize: 100,
+        footerSize: 50
     });
 
-    var logoModifier = new StateModifier({
-        origin: [0.5, 0.5]
-    });
+    layout.header.add(new Surface({
+        content: "Header",
+        classes: ["red-bg"],
+        properties: {
+            lineHeight: "100px",
+            textAlign: "center"
+        }
+    }));
 
-    mainContext.add(logoModifier).add(logo);
+    layout.content.add(new Surface({
+        content: "Content",
+        classes: ["grey-bg"],
+        properties: {
+            lineHeight: '400px',
+            textAlign: "center"
+        }
+    }));
+
+    layout.footer.add(new Surface({
+        content: "Footer",
+        classes: ["red-bg"],
+        properties: {
+            lineHeight: "50px",
+            textAlign: "center"
+        }
+    }));
+
+    mainContext.add(layout);
 });
